4204e760ade83ad868aa1221f326129b4610f47f
Carolina Mesquita <91262204+CarolinaQATester@users.noreply.github.com>
2022-04-06 14:23:55 -0300

    criação da branch de desenv e o arquivo gitignore

---
+++ src/test/java/InicioSelenium.java
@@ -0,0 +1,9 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class InicioSelenium {
+  public static void main(String[] args) {
+      //System.setProperty("webdriver.chrome.drive", "C:/Users/Carol/Documents/driver/chromedriver.exe");
+      ChromeDriver driver = new ChromeDriver();
+      
+  } 
+}
